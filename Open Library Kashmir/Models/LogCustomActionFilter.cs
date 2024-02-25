98b13419f259cd66df71d35c213e5a1783d16471
Malik Umair <malikumairit@gmail.com>
2024-02-25 13:32:17 +0530

    Complete implementation of Custom Helper for displaying images and Logging Custom Action Filter Implementation

---
+++ Open Library Kashmir/Models/LogCustomActionFilter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Open_Library_Kashmir.Models
+{
+    public class LogCustomActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Log("OnActionExecuted", filterContext.RouteData);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Log("OnActionExecuting", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Log("OnResultExecuting", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Log("OnResultExecuted", filterContext.RouteData);
+        }
+
+        private void Log(string methodName, RouteData routeData)
+        {
+            var controllerName = routeData.Values["controller"];
+            var actionName = routeData.Values["action"];
+
+            string message = methodName + " Controller:" + controllerName + " Action:" + actionName + " Date: "
+                            + DateTime.Now.ToString() + Environment.NewLine;
+
+            //saving the data in a text file called LogActions.txt
+            File.AppendAllText(HttpContext.Current.Server.MapPath("~/Log/LogActions.txt"), message);
+        }
+    }
+}
