21b7e7cfdb055c60b2b7394d588071933ced4d92
Malik Umair <malikumairit@gmail.com>
2024-02-26 09:24:09 +0530

    Add automatic migrations functionality to ApplicationDBContext which relates to User Identity

---
+++ Open Library Kashmir/Migrations/202402251238476_202402251807_RemoveTestColumnsFromAspNetUsersTable.cs
@@ -0,0 +1,22 @@
+﻿namespace Open_Library_Kashmir.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _202402251807_RemoveTestColumnsFromAspNetUsersTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.AspNetUsers", "FirstName");
+            DropColumn("dbo.AspNetUsers", "LastName");
+            DropColumn("dbo.AspNetUsers", "BirthDate");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "BirthDate", c => c.DateTime());
+            AddColumn("dbo.AspNetUsers", "LastName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String());
+        }
+    }
+}
