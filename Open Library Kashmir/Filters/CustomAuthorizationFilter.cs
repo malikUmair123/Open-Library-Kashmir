53ae608dff0077cbde7265f89f0bc4d6fb07be44
Malik Umair <malikumairit@gmail.com>
2024-02-26 09:22:15 +0530

    Add Custom Authentication and Custom Authorisation Filters in a new folder called Filters

---
+++ Open Library Kashmir/Filters/CustomAuthorizationFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Open_Library_Kashmir.Filters
+{
+    public class CustomAuthorizationFilter : FilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (String.IsNullOrEmpty(Convert.ToString(filterContext.HttpContext.Session["UserID"])))
+            {
+                filterContext.Result = new ViewResult()
+                {
+                    ViewName = "Error" 
+                };
+            }
+        }
+    }
+}
