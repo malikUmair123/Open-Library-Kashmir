39b23eea6a359dbf048a981ecb93e5131458e6e4
Malik Umair <malikumairit@gmail.com>
2024-02-29 17:27:03 +0530

    Add sign-in with Google functionality

--- Open Library Kashmir/Models/AccountViewModels.cs
+++ Open Library Kashmir/Models/AccountViewModels.cs
@@ -5,6 +5,13 @@ namespace Open_Library_Kashmir.Models
 {
     public class ExternalLoginConfirmationViewModel
     {
+        [Display(Name = "First Name")]
+        public string FirstName {  get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
