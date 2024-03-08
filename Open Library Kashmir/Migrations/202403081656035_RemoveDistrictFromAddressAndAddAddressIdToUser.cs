20441ea9cff8780e6ab126598e6e03b029d3b2b3
Malik Umair <malikumairit@gmail.com>
2024-03-09 01:44:47 +0530

    Removed District field because city represents the same and added AddressId referencing the Address to Application User

---
+++ Open Library Kashmir/Migrations/202403081656035_RemoveDistrictFromAddressAndAddAddressIdToUser.cs
@@ -0,0 +1,30 @@
+﻿namespace Open_Library_Kashmir.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemoveDistrictFromAddressAndAddAddressIdToUser : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.User", "Address_AddressId", "dbo.Address");
+            DropIndex("dbo.User", new[] { "Address_AddressId" });
+            RenameColumn(table: "dbo.User", name: "Address_AddressId", newName: "AddressId");
+            AlterColumn("dbo.User", "AddressId", c => c.Int(nullable: false));
+            CreateIndex("dbo.User", "AddressId");
+            AddForeignKey("dbo.User", "AddressId", "dbo.Address", "AddressId", cascadeDelete: true);
+            DropColumn("dbo.Address", "District");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Address", "District", c => c.String(maxLength: 100));
+            DropForeignKey("dbo.User", "AddressId", "dbo.Address");
+            DropIndex("dbo.User", new[] { "AddressId" });
+            AlterColumn("dbo.User", "AddressId", c => c.Int());
+            RenameColumn(table: "dbo.User", name: "AddressId", newName: "Address_AddressId");
+            CreateIndex("dbo.User", "Address_AddressId");
+            AddForeignKey("dbo.User", "Address_AddressId", "dbo.Address", "AddressId");
+        }
+    }
+}
--- Open Library Kashmir/Models/Address.cs
+++ Open Library Kashmir/Models/Address.cs
@@ -2,6 +2,7 @@ namespace Open_Library_Kashmir.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -11,20 +12,20 @@ namespace Open_Library_Kashmir.Models
     {
         public int AddressId { get; set; }
 
+        [DisplayName("Address Line 1")]
         [StringLength(100)]
         public string AddressLine1 { get; set; }
 
+        [DisplayName("Tehsil")]
         [StringLength(100)]
         public string AddressLine2 { get; set; }
 
         [StringLength(100)]
         public string Locality { get; set; }
 
+        [DisplayName("District")]
         [StringLength(100)]
         public string City { get; set; }
-
-        [StringLength(100)]
-        public string District { get; set; }
 
         [StringLength(100)]
         public string State { get; set; }
@@ -32,6 +33,7 @@ namespace Open_Library_Kashmir.Models
         [StringLength(2)]
         public string CountryCode { get; set; }
 
+        [DisplayName("Pin Code")]
         [StringLength(20)]
         public string PostalCode { get; set; }
     }
