653b34e3c4bb78ba239c24be7592bc2222a81b24
Malik Umair <malikumairit@gmail.com>
2024-03-10 02:27:35 +0530

    Add Database in Azure Cloud And Directed  ApplicationDBContext to it
    Added migrations to remote azure

---
+++ Open Library Kashmir/Migrations/202403092028337_TestAzureDatabaseMigration.cs
@@ -0,0 +1,16 @@
+﻿namespace Open_Library_Kashmir.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TestAzureDatabaseMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
