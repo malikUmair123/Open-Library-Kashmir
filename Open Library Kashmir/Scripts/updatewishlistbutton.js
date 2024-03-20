4ed5648f61689a12156bfa74be91e637fed1a3c5
Malik Umair <malikumairit@gmail.com>
2024-03-20 23:26:40 +0530

    Update Wishlist pages and buttons
    Wishlist buttons need to update from DB once order is placed and buttons need to be disabled with a message, add hover color to Learn more button on home

--- Open Library Kashmir/Scripts/updatewishlistbutton.js
+++ Open Library Kashmir/Scripts/updatewishlistbutton.js
@@ -1,21 +1,25 @@
 ﻿$(document).ready(function () {
-    // Check TempData["BookInWishlist"] value
-    var bookInWishlist = '@TempData["BookInWishlist"]';
+    // Retrieve TempData values
+    var bookInWishlist = '@TempData["BookInWishlist"]' === 'True';
+    var booksInWishlistDB = '@TempData["BooksInWishlistDB"]' === 'True';
 
-    // Update properties of "Add to Wishlist" button based on TempData value
-    if (bookInWishlist === 'True') {
-        // Book is already in wishlist
-        var bookInWishlist = '@TempData["BookInWishlist"]';
+    // Update "Add to Wishlist" button
+    var addToWishlistButton = $('#add-to-wishlist-btn');
 
-        $('#add-to-wishlist-btn').prop('disabled', true);
-        $('#add-to-wishlist-btn').text('Added to Wishlist');
-        $('#add-to-wishlist-btn').removeClass('btn-primary').addClass('btn-success');
+    if (bookInWishlist || booksInWishlistDB) {
+        addToWishlistButton.prop('disabled', true);
+        addToWishlistButton.text('Added to Wishlist');
+        addToWishlistButton.removeClass('btn-primary').addClass('btn-success');
     } else {
-        var bookInWishlist = '@TempData["BookInWishlist"]';
-
-        // Book is not in wishlist
-        $('#add-to-wishlist-btn').prop('disabled', false);
-        $('#add-to-wishlist-btn').text('Add to Wishlist');
-        $('#add-to-wishlist-btn').removeClass('btn-success').addClass('btn-primary');
+        addToWishlistButton.prop('disabled', false);
+        addToWishlistButton.text('Add to Wishlist');
+        addToWishlistButton.removeClass('btn-success').addClass('btn-primary');
     }
+
+    if (booksInWishlistDB) {
+        $('#message-area').text("You have requested some books, thus new ones can't be added.");
+    } else {
+        $('#message-area').hide();
+    }
+
 });
