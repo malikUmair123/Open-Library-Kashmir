9555412a0e55d953ba8a26bcf911a569c0024b94
Malik Umair <malikumairit@gmail.com>
2024-03-22 12:49:53 +0530

    Remove Unity assemblies as of now

--- Open Library Kashmir/App_Start/UnityConfig.cs
+++ Open Library Kashmir/App_Start/UnityConfig.cs
@@ -1,49 +1,49 @@
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.AspNet.Identity;
-using Microsoft.Owin.Security;
-using Open_Library_Kashmir.Models;
-using System.Web.Mvc;
-using Unity;
-using Unity.Injection;
-using Unity.Lifetime;
-using Unity.Mvc5;
-using System.Web;
-using Open_Library_Kashmir.Controllers;
-using System.Web.UI.WebControls;
-using System.Data.Entity;
-using AutoMapper;
+//using Microsoft.AspNet.Identity.EntityFramework;
+//using Microsoft.AspNet.Identity.Owin;
+//using Microsoft.AspNet.Identity;
+//using Microsoft.Owin.Security;
+//using Open_Library_Kashmir.Models;
+//using System.Web.Mvc;
+//using Unity;
+//using Unity.Injection;
+//using Unity.Lifetime;
+//using Unity.Mvc5;
+//using System.Web;
+//using Open_Library_Kashmir.Controllers;
+//using System.Web.UI.WebControls;
+//using System.Data.Entity;
+//using AutoMapper;
 
-namespace Open_Library_Kashmir
-{
-    public static class UnityConfig
-    {
-        public static void RegisterComponents()
-        {
-            var container = new UnityContainer();
+//namespace Open_Library_Kashmir
+//{
+//    public static class UnityConfig
+//    {
+//        public static void RegisterComponents()
+//        {
+//            var container = new UnityContainer();
 
-            // Create a new MapperConfiguration
-            var config = new MapperConfiguration(cfg =>
-            {
-                cfg.CreateMap<ApplicationUser, RecipientViewModel>();
-            });
+//            // Create a new MapperConfiguration
+//            var config = new MapperConfiguration(cfg =>
+//            {
+//                cfg.CreateMap<ApplicationUser, RecipientViewModel>();
+//            });
 
-            // Register IMapper instance
-            container.RegisterInstance(config.CreateMapper());
+//            // Register IMapper instance
+//            container.RegisterInstance(config.CreateMapper());
 
-            //perhaps in Startup.Auth.cs need to register components instead of creating OwinContext
-            #region OWIN
-            container.RegisterType<DbContext, ApplicationDbContext>(new HierarchicalLifetimeManager());
-            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(new HierarchicalLifetimeManager());
-            container.RegisterType<ApplicationUserManager>(new HierarchicalLifetimeManager());
-            container.RegisterFactory<IAuthenticationManager>(
-                c => HttpContext.Current.GetOwinContext().Authentication);
-            container.RegisterType<ApplicationSignInManager>(new HierarchicalLifetimeManager());
-            container.RegisterType<ApplicationRoleManager>(new HierarchicalLifetimeManager());
-            #endregion
+//            //perhaps in Startup.Auth.cs need to register components instead of creating OwinContext
+//            #region OWIN
+//            container.RegisterType<DbContext, ApplicationDbContext>(new HierarchicalLifetimeManager());
+//            container.RegisterType<IUserStore<ApplicationUser>, UserStore<ApplicationUser>>(new HierarchicalLifetimeManager());
+//            container.RegisterType<ApplicationUserManager>(new HierarchicalLifetimeManager());
+//            container.RegisterFactory<IAuthenticationManager>(
+//                c => HttpContext.Current.GetOwinContext().Authentication);
+//            container.RegisterType<ApplicationSignInManager>(new HierarchicalLifetimeManager());
+//            container.RegisterType<ApplicationRoleManager>(new HierarchicalLifetimeManager());
+//            #endregion
 
-            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+//            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
 
-        }
-    }
-}
+//        }
+//    }
+//}
