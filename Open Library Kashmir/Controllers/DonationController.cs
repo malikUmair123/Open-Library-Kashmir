7e38f8006ea30a32c35672ce10e108b05c0e5879
Malik Umair <malikumairit@gmail.com>
2024-04-07 21:11:28 +0530

    Optimize Donation Page

--- Open Library Kashmir/Controllers/DonationController.cs
+++ Open Library Kashmir/Controllers/DonationController.cs
@@ -19,6 +19,7 @@ using System.Web.Routing;
 using System.Web.Services.Description;
 using Twilio.TwiML.Messaging;
 using static Open_Library_Kashmir.Controllers.ManageController;
+using Open_Library_Kashmir.Helpers;
 
 namespace Open_Library_Kashmir.Controllers
 {
@@ -30,6 +31,7 @@ namespace Open_Library_Kashmir.Controllers
         private readonly ApplicationDbContext _context;
         private ApplicationUserManager _userManager;
         private IMapper _mapper;
+        private Wishlist _wishlistInDB;
        
         public DonationController()
         {
@@ -54,20 +56,35 @@ namespace Open_Library_Kashmir.Controllers
             }
         }
 
+        public Wishlist WishListInDB
+        {
+            get
+            {
+                _wishlistInDB = Session["WishlistInDB"] as Wishlist;
+
+                if (_wishlistInDB == null)
+                {
+                    _wishlistInDB = WishlistInDB();
+                    Session["WishlistInDB"] = _wishlistInDB;
+                }
+
+                return _wishlistInDB;
+            }
+            private set
+            {
+                _wishlistInDB = value;
+            }
+        }
+
 
         // GET: Donation
 
         //[OutputCache(CacheProfile = "1MinuteCache", Location = System.Web.UI.OutputCacheLocation.Client)]
         //[OutputCache(Duration = 60, Location = System.Web.UI.OutputCacheLocation.Client)]
-        //public ActionResult Index()
-        //{
-        //    var books = _context.Books.ToList();
-        //    return View(books);
-        //}
 
         public ActionResult Index(string searchString, int? page)
         {
-            var books = from b in _context.Books select b;
+            var books = _context.Books.AsQueryable();
 
             if (!String.IsNullOrEmpty(searchString))
             {
@@ -93,24 +110,20 @@ namespace Open_Library_Kashmir.Controllers
         {
             Book book = _context.Books.FirstOrDefault(x => x.BookId == id);
 
-            //Books in request pipeline
-       
-            TempData["BooksInWishlistDB"] = WishlistInDB() != null;
-          
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
 
-            // Retrieve wishlist from the session
             var wishlist = Session["Wishlist"] as Wishlist;
+            
             if (wishlist == null)
             {
-                wishlist = new Wishlist();
+                wishlist = WishListInDB ?? new Wishlist();
                 Session["Wishlist"] = wishlist;
             }
 
-            // Set TempData with the result of your wishlist session
-            // or if wishlist already exists in database
-            // don't allow the user to add to wishlist
-            // updates properties of Add To Wishlist Button to disables
-            TempData["BookInWishlist"] = wishlist.Books.Any(b => b.BookId == id) /*|| WishlistInDB() != null*/;
+            TempData["BookInWishlist"] = wishlist.Books.Any(b => b.BookId == id);
 
             return View(book);
         }
@@ -121,9 +134,10 @@ namespace Open_Library_Kashmir.Controllers
         {
             // Retrieve wishlist from the session
             var wishlist = Session["Wishlist"] as Wishlist;
+            
             if (wishlist == null)
             {
-                wishlist = new Wishlist();
+                wishlist = WishListInDB ?? new Wishlist();
                 Session["Wishlist"] = wishlist;
             }
 
@@ -131,20 +145,13 @@ namespace Open_Library_Kashmir.Controllers
             if (!wishlist.Books.Any(b => b.BookId == book.BookId))
             {
                 wishlist.Books.Add(book);
-                //_context.SaveChanges(); // Assuming you want to persist in an actual wishlist later
             }
             return RedirectToAction("BookDetails", new { id = book.BookId });
         }
 
         public JsonResult GetWishlistCount()
         {
-            var wishlist = Session["Wishlist"] as Wishlist;
-
-            if (wishlist == null || wishlist.Books.Count == 0)
-            {
-                wishlist = WishlistInDB();
-            }
-
+            var wishlist = Session["Wishlist"] as Wishlist ?? WishlistInDB();
             int count = wishlist != null ? wishlist.Books.Count : 0;
             return Json(new { Count = count }, JsonRequestBehavior.AllowGet);
         }
@@ -152,12 +159,10 @@ namespace Open_Library_Kashmir.Controllers
 
         // Wishlist View 
         public ActionResult Wishlist()
-        {
-            //Books in request pipeline
+        {            
 
-            TempData["BooksInWishlistDB"] = false;
-
-            var wishlist = Session["Wishlist"] as Wishlist;
+            // Retrieve wishlist from the session
+            var wishlist = Session["Wishlist"] as Wishlist ?? WishListInDB;
 
             var wishlistViewModel = new WishlistViewModel();
 
@@ -166,22 +171,10 @@ namespace Open_Library_Kashmir.Controllers
                 // Fetch books from wishlist Session
                 wishlistViewModel.Books = wishlist.Books.ToList();
 
-            } 
-            else
-            {
-                wishlist = WishlistInDB();
-
-                if (wishlist != null)
-                {
-                    TempData["BooksInWishlistDB"] = true;
-                    // Fetch books from wishlist DB
-                    wishlistViewModel.Books = wishlist.Books.ToList();
-                }
-               
             }
 
+            TempData["BooksInWishlistDB"] = WishListInDB != null ? true : false;
 
-            
             return View(wishlistViewModel);
 
         }
@@ -196,33 +189,12 @@ namespace Open_Library_Kashmir.Controllers
                 { 
                     user.Address = _context.Addresses.Create();
                 }
-                // Create a new MapperConfiguration
-                var config = new MapperConfiguration(cfg =>
-                {
-                    // Configure mappings here
-                    cfg.CreateMap<ApplicationUser, RecipientViewModel>()
-                            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
-                            .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
-                            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-                            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
-                            .ForMember(dest => dest.Remarks, opt => opt.MapFrom(src => src.Remarks))
-                            .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address));
-                });
 
-                // Create a new IMapper instance from the MapperConfiguration
-                _mapper = config.CreateMapper();
+                _mapper = Helpers.Helpers.GetMapper();
 
                 if (user != null)
                 {
                     RecipientViewModel recipientViewModel = _mapper.Map<ApplicationUser, RecipientViewModel>(user);
-
-                    ////RecipientViewModel recipientViewModel = new RecipientViewModel()
-                    ////{
-                    ////    Email = user.Email,
-                    ////    First_Name = user.FirstName,
-                    ////    Last_Name = user.LastName,
-                    ////    Phone = user.PhoneNumber
-                    ////};
 
                     return View(recipientViewModel);
                 }
@@ -242,28 +214,6 @@ namespace Open_Library_Kashmir.Controllers
                     string userId = User.Identity.GetUserId();
                     ApplicationUser user = UserManager.FindById(userId);
 
-                    //// Create a new MapperConfiguration
-                    //var config = new MapperConfiguration(cfg =>
-                    //{
-                    //    // Configure mappings here
-                    //    cfg.CreateMap<RecipientViewModel, ApplicationUser>()
-                    //        .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
-                    //        .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
-                    //        .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-                    //        .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
-                    //        .ForMember(dest => dest.Remarks, opt => opt.MapFrom(src => src.Remarks))
-                    //        .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
-                    //        .ForAllOtherMembers(opt => opt.Condition(src => src != null)); 
-
-                    //        //.ForAllOtherMembers(opt => opt.Ignore()); // Ignore existing properties if not provided in the source
-                    //        ////.ForAllOtherMembers(opt => opt.UseDestinationValue()); // Use destination value if source value is null
-                    //});
-
-                    //// Create a new IMapper instance from the MapperConfiguration
-                    //_mapper = config.CreateMapper();
-
-                    //user = _mapper.Map<RecipientViewModel, ApplicationUser>(recipientViewModel);
-
                     //Mapper doesn't update user but ovverides it, user manual mapping as of now
                     user.FirstName = recipientViewModel.FirstName;
                     user.LastName = recipientViewModel.LastName;
@@ -274,14 +224,6 @@ namespace Open_Library_Kashmir.Controllers
 
                     _context.Addresses.AddOrUpdate(recipientViewModel.Address);
                     _context.SaveChanges(); // Save changes to the database
-
-
-                    //if (user != null)
-                    //{
-                    //    _context.Users.AddOrUpdate(user);
-                    //    _context.SaveChanges();
-
-                    //}
 
                     // Save changes to the database
                     var result = await UserManager.UpdateAsync(user);
@@ -336,7 +278,6 @@ namespace Open_Library_Kashmir.Controllers
 
                         //Final save
 
-                        //TempData["SuccessMessage"] = "Your wishlist request has been processed!";
                         return RedirectToAction("RequestSummary", new { Message = DonationMessageId.SuccessMessage });
 
                     }
@@ -349,14 +290,6 @@ namespace Open_Library_Kashmir.Controllers
                 }
                 else
                 {
-                    //Implement later
-
-                    //var wishlist = Session["Wishlist"] as Wishlist;
-                    //if (wishlist != null)
-                    //{
-                    //    TempData["WishlistData"] = wishlist; // Store wishlist for later
-                    //}
-                    //return RedirectToAction("Index", "Home");
                     return RedirectToAction("RequestSummary", new { Message = DonationMessageId.NotImplemented});
 
                 }
@@ -375,87 +308,6 @@ namespace Open_Library_Kashmir.Controllers
 
         public ActionResult RequestSummary(DonationMessageId message)
         {
-            //////need to add wishlist to recipeint which first needs to add books
-            ////        var recipient = CreateRecipient(userId);
-
-            ////        var wishlist = Session["Wishlist"] as Wishlist;
-
-            ////        if (wishlist != null && recipient != null)
-            ////        {
-            ////            // Set recipient ID in wishlist
-            ////            wishlist.RecipientId = recipient.RecipientId;
-
-            ////            recipient.RequestStatus = RequestStatus.Pending;
-            ////            // Add wishlist to recipient's collection
-            ////            //recipient.Wishlists.Add(wishlist);
-            ////            wishlist.Recipient = recipient;
-            ////            // Add or update wishlist and recipient in the database
-            ////            _context.Wishlists.AddOrUpdate(wishlist);
-            ////            //_context.Recipients.AddOrUpdate(recipient);
-
-            ////            // Save changes to the database
-            ////            var finalresult = _context.SaveChanges();
-
-            ////            if (finalresult > 0)
-            ////            {
-            ////                wishlist = null;
-            ////            }
-
-            ////        // ... (Distribution Logic for actual distribution...see if you only need wishlist table
-
-            ////        //foreach (var books in wishlist.Books)
-            ////        //{
-            ////        //    var recipientBook = new RecipientBook
-            ////        //    {
-            ////        //        RecipientId = recipient.RecipientId,
-            ////        //        BookId = books.BookId,
-            ////        //        DateRecieved = DateTime.Now
-            ////        //    };
-            ////        //_context.RecipientBooks.AddOrUpdate(recipientBook);
-            ////        //}
-
-            ////        //Final save
-
-            ////        //TempData["SuccessMessage"] = "Your wishlist request has been processed!";
-            ////        return RedirectToAction("RequestSummary", new { Message = DonationMessageId.SuccessMessage });
-
-            ////        }
-            ////        else
-            ////        {
-            ////            //TempData["ErrorMessage"] = "Wishlist not found for current user.";
-            ////            return RedirectToAction("RequestSummary", new { Message = DonationMessageId.ErrorMessage });
-
-            ////        }
-            ///
-            //if (message == DonationMessageId.SuccessMessage)
-            //{
-            //    // Send an email with this link
-            //    string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-            //    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-            //    string emailBody = @"
-            //            <p>Dear " + model.FirstName + @",</p>
-            //            <p>Thank you for registering with Open Library Kashmir (OLK)!</p>
-            //            <p>To complete the registration process and access your account, please confirm your email address by clicking the link below:</p>
-            //            <p><a href=""" + callbackUrl + @""">Confirm Email</a></p>
-            //            <p>If you did not register with OLK or believe you received this email in error, please disregard it.</p>
-            //            <p>Thank you,</p>
-            //            <p>Open Library Kashmir (OLK) Team</p>";
-
-            //    //SMS Test
-
-            //    //await UserManager.SendSmsAsync(user.PhoneNumber, callbackUrl);
-
-            //    //SendGrid Implementation...see IdentityConfig.cs EmailService
-
-            //    //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-            //    //Smtp...Gmail Implementation
-            //    bool IsSendEmail = Helpers.Helpers.EmailSend(model.Email, "Confirm Email", emailBody, true);
-
-            //    if (IsSendEmail)
-            //    {
-            //        return RedirectToAction("ConfirmEmailConfirmation");
-            //    }
 
                 ViewBag.StatusMessage =
                 message == DonationMessageId.SuccessMessage ? "Thank you for placing the request for books. We will contact you on your email/phone. Check Status of your request on Request Status page"
@@ -473,7 +325,7 @@ namespace Open_Library_Kashmir.Controllers
             if (Request.IsAuthenticated)
             {
                     
-                    var wishlist = WishlistInDB();
+                    var wishlist = WishListInDB;
 
                     if (wishlist != null) 
                     {
@@ -504,21 +356,9 @@ namespace Open_Library_Kashmir.Controllers
                 {
                     user.Address = _context.Addresses.Create();
                 }
-                // Create a new MapperConfiguration
-                var config = new MapperConfiguration(cfg =>
-                {
-                    // Configure mappings here
-                    cfg.CreateMap<ApplicationUser, RecipientViewModel>()
-                            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName))
-                            .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
-                            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-                            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
-                            .ForMember(dest => dest.Remarks, opt => opt.MapFrom(src => src.Remarks))
-                            .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address));
-                });
 
                 // Create a new IMapper instance from the MapperConfiguration
-                _mapper = config.CreateMapper();
+                _mapper = Helpers.Helpers.GetMapper();
 
                 if (user != null)
                 {
@@ -591,7 +431,7 @@ namespace Open_Library_Kashmir.Controllers
 
         public Wishlist WishlistInDB()
         {
-            if (Request.IsAuthenticated)
+            if (User.Identity.IsAuthenticated)
             {
                 var userId = User.Identity.GetUserId();
 
