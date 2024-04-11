75d8640adde268bc369e8203b041af78a9bf29db
Malik Umair <malikumairit@gmail.com>
2024-04-11 12:29:30 +0530

    Add Delete and Edit Book Functionality

---
+++ Open Library Kashmir/Scripts/DeleteBookPopup.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('.delete-btn').click(function () {
+
+        var bookId = $(this).attr('id');
+        var confirmation = confirm("Are you sure you want to delete this book?");
+        if (confirmation) {
+            $.ajax({
+                url: '@Url.Action("DeleteBook", "Donation")',
+                type: 'POST',
+                data: { id: bookId },
+                success: function (result) {
+                    // Optionally, you can handle success response
+                    // For example, remove the row from the table
+                    $('#row_' + bookId).remove();
+                },
+                error: function (xhr, status, error) {
+                    // Optionally, you can handle error response
+                    console.error(xhr.responseText);
+                }
+            });
+        }
+    });
+});
